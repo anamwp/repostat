bba6a1377e5ca34eabe1704a4d801d8ce3f1c39f
Md. Anam Hossain <anam@themexpert.com>
2021-03-05 14:41:42 +0600

    responsive gap updated for grid gap of repo info

--- components/Reposummery.js
+++ components/Reposummery.js
@@ -7,7 +7,7 @@ import Yesterdaycommit from './Yesterdaycommit';
 export default function Reposummery(props) {
     const repo = props.repo;
     return (
-        <div className="grid gid-cols-1 sm:grid-cols-2 p-10 md:grid-cols-4 sm:gap-2 md:gap-0  mt-5 md:mt-5 rounded overflow-hidden">
+        <div className="grid gid-cols-1 sm:grid-cols-2 p-10 md:grid-cols-4 gap-2 sm:gap-2 md:gap-0  mt-5 md:mt-5 rounded overflow-hidden">
             <Totalcommit repo={repo} />
             <Weeklycommit repo={repo} />
             <Todaycommit repo={repo} />
